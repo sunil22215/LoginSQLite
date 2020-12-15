0a67401ad1d0d6e578d4833ae4dcc21181918fde
sunil <sunilkhedkar22215@gmail.com>
2020-12-15 22:17:54 +0530

    change name

--- app/src/main/java/com/example/loginsqlite/SplashScreen.java
+++ app/src/main/java/com/example/loginsqlite/SplashScreen.java
@@ -1,17 +1,16 @@
 package com.example.loginsqlite;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 
-public class PlashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plash_screen);
+        setContentView(R.layout.activity_splash_screen);
         Thread thread = new Thread(){
             public  void run(){
                 try {
@@ -20,7 +19,7 @@ public class PlashScreen extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 finally {
-                    Intent intent = new Intent(PlashScreen.this,MainActivity.class);
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
                     startActivity(intent);
                 }
             }
